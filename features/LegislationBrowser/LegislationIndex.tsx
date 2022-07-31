5ae0455b79a58066f1b9d5d16a9c4ebdf6261d5c
Hugh Rawlinson <hughr2005@gmail.com>
2022-07-31 17:16:42 +0200

    Add legislation modal

--- features/LegislationBrowser/LegislationIndex.tsx
+++ features/LegislationBrowser/LegislationIndex.tsx
@@ -2,6 +2,7 @@ import { getLegislation } from "../OireachtasAPI";
 import { PaginationControls } from "./PaginationControls";
 import { Legislation } from ".";
 import { LegislationIndexFilterForm } from "./LegislationIndexFilterForm";
+import { useState } from "react";
 
 export interface LegislationIndexProps {
   legislation: Awaited<ReturnType<typeof getLegislation>>;
@@ -14,10 +15,10 @@ export default function LegislationIndex({
   skip,
   legislation,
 }: LegislationIndexProps) {
-  console.log(legislation.results.map((result) => result.bill.shortTitleEn));
+  const [highlightBill, setHighlightBill] = useState<string | null>();
 
   return (
-    <div>
+    <div style={{ position: "relative" }}>
       <h1>Legislation</h1>
       <div
         style={{
@@ -27,10 +28,10 @@ export default function LegislationIndex({
           gap: "36px",
         }}
       >
-        <div style={{ minWidth: "270px", flex: "1" }}>
+        <div style={{ minWidth: `${288 - 36}px`, flex: "1" }}>
           <LegislationIndexFilterForm limit={limit} skip={skip} />
         </div>
-        <div style={{ flex: "5" }}>
+        <div style={{ flex: "5", position: "relative" }}>
           <PaginationControls
             limit={limit}
             skip={skip}
@@ -39,16 +40,63 @@ export default function LegislationIndex({
           {legislation.results
             //.filter((legislation) => legislation.bill.act)
             .map((legislation) => (
-              <Legislation
-                key={legislation.bill.shortTitleEn}
-                {...legislation}
-              />
+              <div key={legislation.bill.uri}>
+                <h3 onClick={() => setHighlightBill(legislation.bill.uri)}>
+                  {legislation.bill.shortTitleEn}
+                </h3>
+                <ul
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    padding: "0",
+                    margin: "0",
+                    gap: "18px",
+                  }}
+                >
+                  <li style={{ listStyle: "none", margin: "9px" }}>
+                    {legislation.bill.act ? "Act" : "Bill"}
+                  </li>
+                  <li style={{ listStyle: "none", margin: "9px" }}>
+                    Source: {legislation.bill.source}
+                  </li>
+                </ul>
+              </div>
             ))}
           <PaginationControls
             limit={limit}
             skip={skip}
             pageDetails={legislation.head}
           />
+          {highlightBill && (
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                backdropFilter: "blur(3px)",
+                width: "100%",
+                height: "100%",
+                padding: "36px 144px",
+              }}
+            >
+              <div
+                style={{
+                  background: "#999E",
+                  padding: "18px",
+                  borderRadius: "3px",
+                }}
+              >
+                <button type="button" onClick={() => setHighlightBill(null)}>
+                  Close
+                </button>
+                <Legislation
+                  {...legislation.results.find(
+                    ({ bill }) => bill.uri === highlightBill
+                  )!}
+                />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
